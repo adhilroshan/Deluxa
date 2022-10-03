e79196ba8c5baaf53b8c4afa1c05e9d181beb79a
adhilroshan <adhilchra@gmail.com>
2022-10-03 23:35:35 +0530

    the easy way

--- lib/auth/auth.dart
+++ lib/auth/auth.dart
@@ -1,7 +1,6 @@
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-
-final String myApiKey = dotenv.env['API_KEY'] ?? "";
-final String myRAT = dotenv.env['RAT'] ?? "";
-final String baseURL = dotenv.env['BASIC_URL'] ?? "";
-final String baseImageURL = dotenv.env['BASIC_IMAGE_URL'] ?? "";
-final String baseVideoURL = dotenv.env['YT_VDO_URL'] ?? "";
+const myApiKey = 'e098d71585fdd40fe07f4496a4876962';
+const myRAT =
+    'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlMDk4ZDcxNTg1ZmRkNDBmZTA3ZjQ0OTZhNDg3Njk2MiIsInN1YiI6IjYwYWJlM2Y1M2QzNTU3MDA0MDNiNDI1ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.IbsMInGIslxentW01XuDBDJRrKqq1Xlm8FCRtZ7Z9BI';
+const baseURL = "https://api.themoviedb.org/3";
+const baseImageURL = "https://image.tmdb.org/t/p/";
+const baseVideoURL = "https://www.youtube.com/watch?v=";
